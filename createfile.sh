cbfa9908ef159c38064f91becc0969b324f7d603
victor <vmoenga36@gmail.com>
2023-03-09 04:25:10 -0800

    My name is Betty

--- createfile.sh
+++ createfile.sh
@@ -3,4 +3,4 @@ clear
 
 read -p "Enter name of file: " filename
 
-vim 0x00-shell_basics/$filename && chmod +x 0x00-shell_basics/$filename
+vim 0x01-shell_permissions/$filename && chmod +x 0x01-shell_permissions/$filename
